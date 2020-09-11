b1daf1e3cff4ccea2fab00a196302f3d894f1742
César <dev.pcesarmata@gmail.com>
2020-09-10 22:48:37 -0500

    Init commit.

---
+++ src/main/java/com/cesarmata/ws42restfulwebservicesdynamicfiltering/Ws42RestfulWebServicesDynamicFilteringApplication.java
@@ -0,0 +1,13 @@
+package com.cesarmata.ws42restfulwebservicesdynamicfiltering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ws42RestfulWebServicesDynamicFilteringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ws42RestfulWebServicesDynamicFilteringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesarmata/ws42restfulwebservicesdynamicfiltering/Ws42RestfulWebServicesDynamicFilteringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesarmata.ws42restfulwebservicesdynamicfiltering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ws42RestfulWebServicesDynamicFilteringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
